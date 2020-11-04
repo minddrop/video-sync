6ea20a09c83efbcb267ca2413b401a793ba8681c
minddrop <20136406+minddrop@users.noreply.github.com>
2020-11-05 06:27:36 +0900

    updated settings

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -2,16 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#recommended-configs
     'eslint:recommended', // https://eslint.org/docs/rules/
     'plugin:@typescript-eslint/recommended', // plugin:@typescript-eslint/eslint-recommended を内包 (https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/configs)
+    'plugin:react/recommended',
 
     // https://github.com/prettier/eslint-config-prettier#installation
     // plugin:prettier/recommended は利用しない (https://prettier.io/docs/en/integrating-with-linters.html)
     'prettier',
     'prettier/@typescript-eslint',
+    'prettier/react'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
